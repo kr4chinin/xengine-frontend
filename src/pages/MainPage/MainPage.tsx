99f377e343831ea66b459a1e7ebacf791137fc7e
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-09 23:07:58 +0300

    fix: extracted header from MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,7 @@
+import CatalogControls from '../../components/CatalogControls/CatalogControls'
 import HorizontalLine from '../../components/HorizontalLine/HorizontalLine'
 import NavBar from '../../components/NavBar/NavBar'
+import PopularVehiclesList from '../../components/PopularVehiclesList/PopularVehiclesList'
 import styles from './MainPage.module.scss'
 
 const MainPage = () => {
@@ -7,8 +9,9 @@ const MainPage = () => {
 		<div className={styles.container}>
 			<NavBar />
             <HorizontalLine marginTop='60px'/>
-            <h2>Most popular vehicles</h2>
-        
+            <PopularVehiclesList />
+            <HorizontalLine marginTop='60px'/>
+            <CatalogControls />
 		</div>
 	)
 }
