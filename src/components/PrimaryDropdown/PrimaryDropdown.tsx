71e492b1e4ba4b283c1c578dd1260f33bf1f0b07
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-09 23:39:06 +0300

    feat: PrimaryDropdown component

---
+++ src/components/PrimaryDropdown/PrimaryDropdown.tsx
@@ -0,0 +1,59 @@
+import styles from './PrimaryDropdown.module.scss'
+import { useState, useRef, FC } from 'react'
+import { useClickOutside } from '../../hooks/useClickOutside'
+import { Icon } from '@iconify/react'
+import cl from 'classnames'
+
+interface PrimaryDropdownProps {
+	list: { id: number; name: string }[]
+    title: string
+}
+
+const PrimaryDropdown: FC<PrimaryDropdownProps> = ({ list, title }) => {
+	const [isShown, setIsShown] = useState(false)
+
+	const triggerRef = useRef<HTMLDivElement>(null)
+	const dropdownRef = useRef<HTMLDivElement>(null)
+
+	function toggleDropdown() {
+		setIsShown(prev => !prev)
+	}
+
+	function handleStopPropagation(e: React.MouseEvent<HTMLDivElement>) {
+		e.stopPropagation()
+	}
+
+	function handleClose() {
+		setIsShown(false)
+	}
+
+	useClickOutside(triggerRef, dropdownRef, handleClose)
+
+	return (
+		<div className={styles.trigger} onClick={toggleDropdown} ref={triggerRef}>
+			<div className={styles.header}>
+				<p>{title}</p>
+				{isShown ? (
+					<Icon icon="akar-icons:arrow-back-thick" className="icon" />
+				) : (
+					<Icon icon="fluent:more-circle-20-filled" />
+				)}
+			</div>
+			<div className={cl(styles.dropdown, { [styles.active]: isShown })}>
+				<div
+					className={cl(styles.menu, { [styles.active]: isShown })}
+					ref={dropdownRef}
+					onClick={handleStopPropagation}
+				>
+					<ul>
+						{list.map(el => (
+							<li key={el.id}>{el.name}</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default PrimaryDropdown
