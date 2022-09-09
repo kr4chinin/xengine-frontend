c72358bc207d03ef0b9cd365238cad759823fb44
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-09 20:57:10 +0300

    feat: font-loader plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 import svgr from 'vite-plugin-svgr'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 
 export default defineConfig({
-  plugins: [react(), eslint(), svgr()]
+	plugins: [
+		react(),
+		eslint(),
+		svgr(),
+		VitePluginFonts({
+			google: {
+				families: ['Poppins']
+			}
+		})
+	]
 })
