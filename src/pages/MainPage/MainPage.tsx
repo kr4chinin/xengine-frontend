c76f55fdb7e5a7a0394af1491e8ec2940cbcc7f2
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-09 21:10:38 +0300

    chore: continue working on NavBar and MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,14 @@
+import HorizontalLine from '../../components/HorizontalLine/HorizontalLine'
 import NavBar from '../../components/NavBar/NavBar'
 import styles from './MainPage.module.scss'
 
 const MainPage = () => {
 	return (
-		<div>
+		<div className={styles.container}>
 			<NavBar />
+            <HorizontalLine marginTop='60px'/>
+            <h2>Most popular vehicles</h2>
+        
 		</div>
 	)
 }
