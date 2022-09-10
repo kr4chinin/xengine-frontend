4d75a9e48024073f4abac6573c83204b9870f068
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-10 12:36:43 +0300

    feat: navbar is changing when scrolling down

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,33 @@
 import styles from './NavBar.module.scss'
-import { ReactComponent as Logo } from '../../assets/images/engine.svg'
+import { ReactComponent as Logo } from '../../assets/icons/engine.svg'
+import cl from 'classnames'
+import { useEffect, FC } from 'react'
 
-const NavBar = () => {
+interface NavBarProps {
+    isVisible: boolean
+    setIsVisible: (isVisible: boolean) => void
+}
+
+const NavBar: FC<NavBarProps> = ({isVisible, setIsVisible}) => {
+
+	useEffect(() => {
+		const toggleVisibility = () => {
+			if (window.pageYOffset > 80) {
+				setIsVisible(true)
+			} else {
+				setIsVisible(false)
+			}
+		}
+
+		document.addEventListener('scroll', toggleVisibility)
+
+		return () => {
+			document.removeEventListener('scroll', toggleVisibility)
+		}
+	}, [setIsVisible])
+
 	return (
-		<nav className={styles.container}>
+		<nav className={cl(styles.container, { [styles.visible]: isVisible })}>
 			<div className={styles['brand-container']}>
 				<Logo className={styles.logo} />
 				<h1 className={styles.brand}>XEngine</h1>
