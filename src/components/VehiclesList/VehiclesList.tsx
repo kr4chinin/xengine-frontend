22931c0b5ac246a8924f05ca214aa6c80775d61b
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-10 23:00:38 +0300

    fix: now VehiclesList getting vehicles as property

--- src/components/VehiclesList/VehiclesList.tsx
+++ src/components/VehiclesList/VehiclesList.tsx
@@ -1,70 +1,20 @@
+import { FC } from 'react'
 import { Vehicle } from '../../types/Vehicle'
 import VehicleItem from '../VehicleItem/VehicleItem'
 import styles from './VehicleList.module.scss'
 
-const VehiclesList = () => {
-	const vehicles: Vehicle[] = [
-		{
-			id: 1,
-			name: 'Vehicle 1',
-			rating: 6.1,
-			price: 2500000,
-			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
-		},
-		{
-			id: 2,
-			name: 'Vehicle 2',
-			rating: 4.0,
-			price: 3500000,
-			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
-		},
-		{
-			id: 3,
-			name: 'Vehicle 3',
-			rating: 5.1,
-			price: 4500000,
-			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
-		},
-		{
-			id: 4,
-			name: 'Vehicle 4',
-			rating: 6.1,
-			price: 2500000,
-			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
-		},
-		{
-			id: 5,
-			name: 'Vehicle 5',
-			rating: 4.0,
-			price: 3500000,
-			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
-		},
-		{
-			id: 6,
-			name: 'Vehicle 6',
-			rating: 5.1,
-			price: 4500000,
-			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-			description:
-				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
-		}
-	]
+interface VehicleListProps {
+	vehicles: Vehicle[]
+}
 
+const VehiclesList: FC<VehicleListProps> = ({ vehicles }) => {
 	return (
-        <div className={styles.container}>
-            {vehicles.map(vehicle => (<VehicleItem vehicle={vehicle} key={vehicle.id}/>))}
-        </div>
-    )
+		<div className={styles.container}>
+			{vehicles.map(vehicle => (
+				<VehicleItem vehicle={vehicle} key={vehicle.id} />
+			))}
+		</div>
+	)
 }
 
 export default VehiclesList
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -6,10 +6,68 @@ import VehiclesList from '../../components/VehiclesList/VehiclesList'
 import styles from './MainPage.module.scss'
 import { useState } from 'react'
 import SideBar from '../../components/sections/SideBar/SideBar'
+import { Vehicle } from '../../types/Vehicle'
 
 const MainPage = () => {
 	const [isNavBarVisible, setIsNavBarVisible] = useState(false)
 	const [isSideBarVisible, setIsSideBarVisible] = useState(false)
+
+    const vehicles: Vehicle[] = [
+		{
+			id: 1,
+			name: 'Vehicle 1',
+			rating: 6.1,
+			price: 2500000,
+			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+		},
+		{
+			id: 2,
+			name: 'Vehicle 2',
+			rating: 4.0,
+			price: 3500000,
+			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+		},
+		{
+			id: 3,
+			name: 'Vehicle 3',
+			rating: 5.1,
+			price: 4500000,
+			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+		},
+		{
+			id: 4,
+			name: 'Vehicle 4',
+			rating: 6.1,
+			price: 2500000,
+			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+		},
+		{
+			id: 5,
+			name: 'Vehicle 5',
+			rating: 4.0,
+			price: 3500000,
+			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+		},
+		{
+			id: 6,
+			name: 'Vehicle 6',
+			rating: 5.1,
+			price: 4500000,
+			img: 'https://images.unsplash.com/photo-1623972860045-f2d0accbcf38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+		}
+	]
 
 	return (
 		<div>
@@ -18,7 +76,7 @@ const MainPage = () => {
 			<PopularVehiclesList />
 			<HorizontalLine marginTop="60px" />
 			<CatalogControls />
-			<VehiclesList />
+			<VehiclesList vehicles={vehicles}/>
 			<SideBar
 				isVisible={isSideBarVisible}
 				setIsVisible={setIsSideBarVisible}
