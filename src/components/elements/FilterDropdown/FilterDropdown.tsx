445dd09e32744c2115e068da264b26178d443172
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-15 16:09:44 +0300

    feat: highlighting current sort option in the menu

--- src/components/elements/FilterDropdown/FilterDropdown.tsx
+++ src/components/elements/FilterDropdown/FilterDropdown.tsx
@@ -7,6 +7,13 @@ import { Icons } from '../../../utils/Icons'
 import vehicle from '../../../store/VehicleStore'
 import { SortBy } from '../../../utils/SortBy'
 
+interface SortOption {
+    id: number
+    name: string
+    sortKey: SortBy
+    onClick: () => void
+}
+
 const FilterDropdown = () => {
 	const [isShown, setIsShown] = useState(false)
 
@@ -27,49 +34,60 @@ const FilterDropdown = () => {
 
 	useClickOutside(triggerRef, dropdownRef, handleClose)
 
-	const options: { id: number; name: string; onClick: () => void }[] = [
+	const options: SortOption[] = [
 		{
 			id: 1,
 			name: 'Price',
+			sortKey: SortBy.PRICE_DESCENDING,
 			onClick: () => vehicle.setSort(SortBy.PRICE_DESCENDING)
 		},
 		{
 			id: 2,
 			name: 'Name',
+			sortKey: SortBy.NAME_ASCENDING,
 			onClick: () => vehicle.setSort(SortBy.NAME_ASCENDING)
 		},
 		{
 			id: 3,
 			name: 'Rating',
+			sortKey: SortBy.RATING_DESCENDING,
 			onClick: () => vehicle.setSort(SortBy.RATING_DESCENDING)
 		}
 	]
 
 	return (
-		<div className={styles.trigger} onClick={toggleDropdown} ref={triggerRef}>
-			<div className={styles.header}>
-				<p>Filter</p>
-				<Icon
-					icon={isShown ? Icons.CLOSE_FILTER : Icons.FILTER}
-					className="icon"
-				/>
-			</div>
-			<div className={cl(styles.dropdown, { [styles.active]: isShown })}>
-				<div
-					className={cl(styles.menu, { [styles.active]: isShown })}
-					ref={dropdownRef}
-					onClick={handleStopPropagation}
-				>
-					<ul>
-						{options.map(option => (
-							<li key={option.id} onClick={option.onClick}>
-								{option.name}
-							</li>
-						))}
-					</ul>
+		<>
+			<div className={styles.trigger} onClick={toggleDropdown} ref={triggerRef}>
+				<div className={styles.header}>
+					<p>Filter</p>
+					<Icon
+						icon={isShown ? Icons.CLOSE_FILTER : Icons.FILTER}
+						className="icon"
+					/>
+				</div>
+				<div className={cl(styles.dropdown, { [styles.active]: isShown })}>
+					<div
+						className={cl(styles.menu, { [styles.active]: isShown })}
+						ref={dropdownRef}
+						onClick={handleStopPropagation}
+					>
+						<ul>
+							{options.map(option => (
+								<li
+									key={option.id}
+									onClick={option.onClick}
+									className={cl({
+										[styles.active]: option.sortKey === vehicle.sort
+									})}
+								>
+									{option.name}
+								</li>
+							))}
+						</ul>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
