84236d8b6fa122572abd8abfbad4b3acb50e617e
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-15 06:36:18 +0300

    feat: handling loading state in DevicePage

--- src/pages/DevicePage/DevicePage.tsx
+++ src/pages/DevicePage/DevicePage.tsx
@@ -8,7 +8,7 @@ import { useState } from 'react'
 import HorizontalLine from '../../components/elements/HorizontalLine/HorizontalLine'
 import { Type } from '../../types/Type'
 import { Brand } from '../../types/Brand'
-import { ThreeDots } from 'react-loader-spinner'
+import { Radio, ThreeDots } from 'react-loader-spinner'
 import AddToCartButton from '../../components/elements/AddToCartButton/AddToCartButton'
 import { Icon } from '@iconify/react'
 import { Icons } from '../../utils/Icons'
@@ -52,65 +52,75 @@ const DevicePage = () => {
 	return (
 		<>
 			<NavBar isVisible={isNavBarVisible} setIsVisible={setIsNavBarVisible} />
-			<div className={styles.container}>
-				<HorizontalLine marginTop="200px" />
-				<div className={styles['main-info']}>
-					<p className={styles.name}>{vehicle?.name}</p>
-					<div className={styles.type}>
-						{!isTypeLoading && !isTypeError && type?.name}
-						{isTypeLoading && (
-							<ThreeDots width={45} height={45} color="#5878A9" />
-						)}
-						{isTypeError && <p>Error 🚫</p>}
-					</div>
-					<div className={styles.brand}>
-						{!isBrandLoading && !isBrandError && brand?.name}
-						{isBrandLoading && (
-							<ThreeDots width={45} height={45} color="#5878A9" />
-						)}
-						{isBrandError && <p>Error 🚫</p>}
-					</div>
+			{isLoading && (
+				<div className={styles['loader-container']}>
+					<Radio
+						colors={['#5878A9', '#ADD1DF', '#5878A9']}
+						height={100}
+						width={200}
+						wrapperStyle={{ alignSelf: 'center' }}
+					/>
 				</div>
-				<div className={styles.content}>
-					<div className={styles['first-column']}>
-						<div className={styles['image-container']}>
-							<img
-								src={import.meta.env.VITE_API_URL + vehicle?.img}
-								alt={vehicle?.name}
-							/>
-							<AddToCartButton height="65px" width="65px" />
+			)}
+			{!isLoading && !isError && (
+				<div className={styles.container}>
+					<HorizontalLine marginTop="200px" />
+					<div className={styles['main-info']}>
+						<p className={styles.name}>{vehicle?.name}</p>
+						<div className={styles.type}>
+							{!isTypeLoading && !isTypeError && type?.name}
+							{isTypeLoading && (
+								<ThreeDots width={45} height={45} color="#5878A9" />
+							)}
+							{isTypeError && <p>Error 🚫</p>}
 						</div>
-						<div className={styles['first-column-additional']}>
-							<div className={styles.price}>
-								<p>
-									<span>Price: </span>
-									{convertPrice(String(vehicle?.price))}
-								</p>
-								<Icon icon={Icons.DOLLAR_CIRCLE} />
-							</div>
-							<div className={styles.rating}>
-								<p>
-									<span>Rating: </span>
-									{vehicle?.rating}
-								</p>
-								<Icon icon={Icons.STAR} />
-							</div>
+						<div className={styles.brand}>
+							{!isBrandLoading && !isBrandError && brand?.name}
+							{isBrandLoading && (
+								<ThreeDots width={45} height={45} color="#5878A9" />
+							)}
+							{isBrandError && <p>Error 🚫</p>}
 						</div>
 					</div>
-					<div className={styles['second-column']}>
-                        <HorizontalLine borderColor='#808080' marginTop='0px'/>
-                        <section className={styles.description}>
-                            <h3>Description: </h3>
-                            <p>
-                                {vehicle?.description}
-                            </p>
-                        </section>
-                        <div className={styles.characteristics}>
-                            <h3>Characteristics: </h3>
-                        </div>
-                    </div>  
+					<div className={styles.content}>
+						<div className={styles['first-column']}>
+							<div className={styles['image-container']}>
+								<img
+									src={import.meta.env.VITE_API_URL + vehicle?.img}
+									alt={vehicle?.name}
+								/>
+								<AddToCartButton height="65px" width="65px" />
+							</div>
+							<div className={styles['first-column-additional']}>
+								<div className={styles.price}>
+									<p>
+										<span>Price: </span>
+										{convertPrice(String(vehicle?.price))}
+									</p>
+									<Icon icon={Icons.DOLLAR_CIRCLE} />
+								</div>
+								<div className={styles.rating}>
+									<p>
+										<span>Rating: </span>
+										{vehicle?.rating}
+									</p>
+									<Icon icon={Icons.STAR} />
+								</div>
+							</div>
+						</div>
+						<div className={styles['second-column']}>
+							<HorizontalLine borderColor="#808080" marginTop="0px" />
+							<section className={styles.description}>
+								<h3>Description: </h3>
+								<p>{vehicle?.description}</p>
+							</section>
+							<div className={styles.characteristics}>
+								<h3>Characteristics: </h3>
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
+			)}
 		</>
 	)
 }
